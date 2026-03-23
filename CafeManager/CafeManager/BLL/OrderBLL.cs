bf3e4df4974e88d0b5d9d9c81efff5d91c8e0212
zunsnguyen-jpg <zunsnguyen@gmail.com>
2026-03-23 22:04:40 +0700

    Add files via upload

---
+++ CafeManager/CafeManager/BLL/OrderBLL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using CafeManager.DAL;
+
+namespace CafeManager.BLL
+{
+    internal class OrderBLL
+    {
+        OrderDAL dal = new OrderDAL();
+
+        public int CreateOrder(int tableId)
+        {
+            return dal.CreateOrder(tableId);
+        }
+
+        public void AddItem(int orderId, int productId, int quantity, decimal price)
+        {
+            dal.AddItem(orderId, productId, quantity, price);
+        }
+
+        public DataTable GetDetails(int orderId)
+        {
+            return dal.GetDetails(orderId);
+        }
+
+        public decimal GetTotal(int orderId)
+        {
+            DataTable dt = dal.GetDetails(orderId);
+            decimal total = 0;
+
+            foreach (DataRow row in dt.Rows)
+            {
+                total += Convert.ToInt32(row["Quantity"]) * Convert.ToDecimal(row["Price"]);
+            }
+
+            return total;
+        }
+
+        public void Pay(int orderId)
+        {
+            dal.Pay(orderId);
+        }
+        public int GetUnpaidOrder(int tableId)
+        {
+            return dal.GetUnpaidOrder(tableId);
+        }
+        public void DeleteItem(int orderDetailId)
+        {
+            dal.DeleteItem(orderDetailId);
+        }
+    }
+
+
+}
