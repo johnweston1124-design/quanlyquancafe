9c3385ece08f97e86153a323c9f70663df5cceb9
johnweston1124-design <johnweston1124@gmail.com>
2026-04-08 19:46:12 +0700

    feat(product): reupdate product

---
+++ frmProduct.cs
@@ -0,0 +1,76 @@
+﻿using CoffeeShop.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quanlyquancafe
+{
+    public partial class frmProduct : Form
+    {
+        public frmProduct()
+        {
+            InitializeComponent();
+            FormatHelper.ConfigDataGridView(dgvData);
+            lblTitle.ForeColor = ThemeHelper.PrimaryColor;
+            btnAdd.BackColor = ThemeHelper.PrimaryColor;
+            btnEdit.BackColor = ThemeHelper.PrimaryColor;
+            btnSave.BackColor = ThemeHelper.PrimaryColor;
+            LoadData();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnlControl_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void LoadData()
+        {
+            var repo = new ProductRepository();
+            dgvData.DataSource = repo.GetAllAvailable(); // hoặc GetAll()
+
+            dgvData.AutoGenerateColumns = false;
+
+        }
+
+        private void frmProduct_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
