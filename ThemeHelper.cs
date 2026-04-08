52bd50859bba6e4bb900ff7a2cb8edd2a78117f2
johnweston1124-design <johnweston1124@gmail.com>
2026-04-08 19:52:50 +0700

    feat(product): reupdate the product

---
+++ ThemeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+public static class ThemeHelper
+{
+    public static Color PrimaryColor = Color.FromArgb(41, 128, 185);
+    public static Color SecondaryColor = Color.FromArgb(52, 152, 219);
+    public static Color BackgroundColor = Color.FromArgb(236, 240, 241);
+    public static Color SidebarColor = Color.FromArgb(44, 62, 80);
+    public static Color White = Color.White;
+    public static Font MainFont = new Font("Segoe UI", 10, FontStyle.Regular);
+    public static Font HeaderFont = new Font("Segoe UI", 12, FontStyle.Bold);
+}
