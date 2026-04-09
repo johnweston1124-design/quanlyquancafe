142e6f889222ae7d76d10855a13d73af17e19237
EminemTrungKi <trantatminh1964@gmail.com>
2026-04-10 03:04:45 +0700

    Add files via upload

--- WindowsFormsApp1/GUI/frmOrder.Designer.cs
+++ WindowsFormsApp1/GUI/frmOrder.Designer.cs
@@ -16,21 +16,25 @@
         private void InitializeComponent()
         {
             this.pnlLeft = new System.Windows.Forms.Panel();
+            this.dgvOrderDetail = new System.Windows.Forms.DataGridView();
             this.pnlRight = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPayment = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.flpProductList = new System.Windows.Forms.FlowLayoutPanel();
+            this.cboCategorySelect = new System.Windows.Forms.ComboBox();
             this.lblTableSelected = new System.Windows.Forms.Label();
             this.lblTitle = new System.Windows.Forms.Label();
             this.pnlHeader = new System.Windows.Forms.Panel();
-            this.dgvOrderDetail = new System.Windows.Forms.DataGridView();
-            this.cboCategorySelect = new System.Windows.Forms.ComboBox();
-            this.flpProductList = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.lblTotal = new System.Windows.Forms.Label();
-            this.btnPayment = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
             this.pnlLeft.SuspendLayout();
-            this.pnlRight.SuspendLayout();
-            this.pnlHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetail)).BeginInit();
+            this.pnlRight.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.pnlHeader.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlLeft
@@ -42,6 +46,17 @@
             this.pnlLeft.Size = new System.Drawing.Size(500, 390);
             this.pnlLeft.TabIndex = 1;
             // 
+            // dgvOrderDetail
+            // 
+            this.dgvOrderDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrderDetail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvOrderDetail.Location = new System.Drawing.Point(0, 0);
+            this.dgvOrderDetail.Name = "dgvOrderDetail";
+            this.dgvOrderDetail.ReadOnly = true;
+            this.dgvOrderDetail.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvOrderDetail.Size = new System.Drawing.Size(500, 390);
+            this.dgvOrderDetail.TabIndex = 0;
+            // 
             // pnlRight
             // 
             this.pnlRight.Controls.Add(this.panel1);
@@ -52,6 +67,59 @@
             this.pnlRight.Name = "pnlRight";
             this.pnlRight.Size = new System.Drawing.Size(300, 390);
             this.pnlRight.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.btnPayment);
+            this.panel1.Controls.Add(this.btnEdit);
+            this.panel1.Controls.Add(this.lblTotal);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 138);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 252);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnPayment
+            // 
+            this.btnPayment.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnPayment.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnPayment.Location = new System.Drawing.Point(0, 192);
+            this.btnPayment.Name = "btnPayment";
+            this.btnPayment.Size = new System.Drawing.Size(300, 60);
+            this.btnPayment.TabIndex = 1;
+            this.btnPayment.Text = "THANH TOÁN";
+            this.btnPayment.UseVisualStyleBackColor = false;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.Red;
+            this.lblTotal.Location = new System.Drawing.Point(6, 14);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(142, 31);
+            this.lblTotal.TabIndex = 0;
+            this.lblTotal.Text = "Tổng Giá:";
+            // 
+            // flpProductList
+            // 
+            this.flpProductList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flpProductList.Location = new System.Drawing.Point(0, 21);
+            this.flpProductList.Name = "flpProductList";
+            this.flpProductList.Size = new System.Drawing.Size(300, 369);
+            this.flpProductList.TabIndex = 1;
+            // 
+            // cboCategorySelect
+            // 
+            this.cboCategorySelect.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cboCategorySelect.FormattingEnabled = true;
+            this.cboCategorySelect.Location = new System.Drawing.Point(0, 0);
+            this.cboCategorySelect.Name = "cboCategorySelect";
+            this.cboCategorySelect.Size = new System.Drawing.Size(300, 21);
+            this.cboCategorySelect.TabIndex = 0;
             // 
             // lblTableSelected
             // 
@@ -83,65 +151,70 @@
             this.pnlHeader.Size = new System.Drawing.Size(800, 60);
             this.pnlHeader.TabIndex = 2;
             // 
-            // dgvOrderDetail
+            // btnAdd
             // 
-            this.dgvOrderDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvOrderDetail.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvOrderDetail.Location = new System.Drawing.Point(0, 0);
-            this.dgvOrderDetail.Name = "dgvOrderDetail";
-            this.dgvOrderDetail.ReadOnly = true;
-            this.dgvOrderDetail.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvOrderDetail.Size = new System.Drawing.Size(500, 390);
-            this.dgvOrderDetail.TabIndex = 0;
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.FlatAppearance.BorderSize = 0;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnAdd.Location = new System.Drawing.Point(12, 132);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(90, 24);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
             // 
-            // cboCategorySelect
+            // btnEdit
             // 
-            this.cboCategorySelect.Dock = System.Windows.Forms.DockStyle.Top;
-            this.cboCategorySelect.FormattingEnabled = true;
-            this.cboCategorySelect.Location = new System.Drawing.Point(0, 0);
-            this.cboCategorySelect.Name = "cboCategorySelect";
-            this.cboCategorySelect.Size = new System.Drawing.Size(300, 21);
-            this.cboCategorySelect.TabIndex = 0;
+            this.btnEdit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEdit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEdit.FlatAppearance.BorderSize = 0;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnEdit.Location = new System.Drawing.Point(183, 132);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(90, 24);
+            this.btnEdit.TabIndex = 13;
+            this.btnEdit.Text = "EDIT";
+            this.btnEdit.UseVisualStyleBackColor = true;
             // 
-            // flpProductList
+            // btnDelete
             // 
-            this.flpProductList.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flpProductList.Location = new System.Drawing.Point(0, 21);
-            this.flpProductList.Name = "flpProductList";
-            this.flpProductList.Size = new System.Drawing.Size(300, 369);
-            this.flpProductList.TabIndex = 1;
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.BackColor = System.Drawing.Color.IndianRed;
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnDelete.Location = new System.Drawing.Point(12, 162);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(90, 24);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = false;
             // 
-            // panel1
+            // btnSave
             // 
-            this.panel1.Controls.Add(this.btnPayment);
-            this.panel1.Controls.Add(this.lblTotal);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 210);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(300, 180);
-            this.panel1.TabIndex = 0;
-            // 
-            // lblTotal
-            // 
-            this.lblTotal.AutoSize = true;
-            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotal.ForeColor = System.Drawing.Color.Red;
-            this.lblTotal.Location = new System.Drawing.Point(6, 14);
-            this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(142, 31);
-            this.lblTotal.TabIndex = 0;
-            this.lblTotal.Text = "Tổng Giá:";
-            // 
-            // btnPayment
-            // 
-            this.btnPayment.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.btnPayment.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.btnPayment.Location = new System.Drawing.Point(0, 120);
-            this.btnPayment.Name = "btnPayment";
-            this.btnPayment.Size = new System.Drawing.Size(300, 60);
-            this.btnPayment.TabIndex = 1;
-            this.btnPayment.Text = "THANH TOÁN";
-            this.btnPayment.UseVisualStyleBackColor = false;
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnSave.Location = new System.Drawing.Point(183, 162);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 24);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
             // 
             // frmOrder
             // 
@@ -152,12 +225,12 @@
             this.Name = "frmOrder";
             this.Text = "frmOrder";
             this.pnlLeft.ResumeLayout(false);
-            this.pnlRight.ResumeLayout(false);
-            this.pnlHeader.ResumeLayout(false);
-            this.pnlHeader.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetail)).EndInit();
+            this.pnlRight.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -172,5 +245,9 @@
         private System.Windows.Forms.Button btnPayment;
         private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.FlowLayoutPanel flpProductList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
     }
 }
