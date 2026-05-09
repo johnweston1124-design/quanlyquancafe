6a640e7183e322d2726d61889cf30e25cf5384e5
johnweston1124-design <johnweston1124@gmail.com>
2026-05-10 00:20:22 +0700

    feat(develop): continue fixing

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 ﻿using quanlyquancafe;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-=======
-﻿using System;
->>>>>>> feature/product
 using System.Windows.Forms;
 
 namespace quanlyquancafe.GUI
@@ -18,12 +14,7 @@ namespace quanlyquancafe.GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new frmLogin());
-=======
-
-            Application.Run(new frmCategory()); // form chính
->>>>>>> feature/product
         }
     }
 }
